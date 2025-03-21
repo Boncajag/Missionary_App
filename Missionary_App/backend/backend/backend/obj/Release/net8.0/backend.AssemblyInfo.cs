fde6a871de8e4c7f9c685eb384b5de78d74a9eba
Tasedit <tasedit@gmail.com>
2025-03-20 20:21:39 -0600

    opening vs code

--- Missionary_App/backend/backend/backend/obj/Release/net8.0/backend.AssemblyInfo.cs
+++ Missionary_App/backend/backend/backend/obj/Release/net8.0/backend.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c93a61e1e18d58a5d3f6ed2a3bf06cea6debcd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
